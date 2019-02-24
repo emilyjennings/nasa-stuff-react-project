1508ea61df6e21a3cc76c197b093aa96594b5961
github username <github email address>
2019-02-23 20:47:25 -0500

    got some ajax working in components on click

---
+++ src/components/Play.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Play extends Component {
+
+
+
+  render() {
+
+    return (
+      <div class="buttoncontainer">
+        <button onClick={ e => this.props.showGame(e)}>Play!</button>
+      </div>
+    );
+  }
+}
---
+++ src/components/SearchImages.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import $ from 'jquery'
+
+export default class SearchImages extends Component {
+  state = {
+    image: "",
+    desc: "",
+    title: ""
+  }
+
+  componentDidMount(){
+    // const spaceSearch = ["moon", "earth", "jupiter", "saturn", "pluto", "mars", "venus"]
+    // let randomSpaceSearch = spaceSearch[random(spaceSearch.length)]
+
+    const url = "https://images-api.nasa.gov/search?q="
+
+    //sending the call to the NASA API
+        $.ajax({
+          url: url + "saturn",
+          type: "GET",
+          dataType : "json",
+        }).done(function(json){
+          let imageres = json.collection.items[0].links[0].href
+        }).then(json => {
+          this.setState({ image: json.collection.items[0].links[0].href })
+        })
+
+        $.ajax({
+          url: url + "pluto",
+          type: "GET",
+          dataType : "json",
+        }).done(function(json){
+
+        }).then(json => {
+          this.setState({ image: json.collection.items[0].links[0].href })
+          this.setState({ desc: json.collection.items[0].data[0].description })
+          this.setState({ title: json.collection.items[0].data[0].title })
+        });
+
+
+      // }).this.setState({ image: imageVariable })
+
+  }
+
+  render() {
+
+    return (
+
+      <div class="image">
+        <h1>{this.state.title}</h1>
+        <img src={this.state.image}></img>
+        <div>{this.state.desc}</div>
+      </div>
+
+    );
+  }
+}
