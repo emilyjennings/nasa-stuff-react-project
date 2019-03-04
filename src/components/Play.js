2e9d6f20637dccbce70a144a9a6def735c6ae089
github username <github email address>
2019-03-04 15:17:59 -0500

    edited buttons added play button to root

--- src/components/Play.js
+++ src/components/Play.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 export default class Play extends Component {
 
-
-
   render() {
 
     return (
--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -3,6 +3,8 @@ import SearchImages from '../components/SearchImages'
 import Search from '../components/Search'
 import Welcome from '../components/Welcome'
 import SearchButton from '../components/SearchButton'
+import Play from '../components/Play'
+import Game from '../components/Game'
 import $ from 'jquery'
 
 class SearchContainer extends Component {
@@ -10,7 +12,8 @@ class SearchContainer extends Component {
 
   state = {
     buttonClicked: false,
-    buttonSearchClicked: false
+    buttonSearchClicked: false,
+    buttonGameClicked: false
   }
 
 
@@ -18,6 +21,13 @@ class SearchContainer extends Component {
     e.preventDefault();
     this.setState({
       buttonSearchClicked: true
+    })
+  }
+
+  showGame = (e) => {
+    e.preventDefault();
+    this.setState({
+      buttonGameClicked: true
     })
   }
 
@@ -30,9 +40,33 @@ class SearchContainer extends Component {
 
         {this.state.buttonSearchClicked == false ? <SearchButton showSearch={this.showSearch} buttonSearchClicked={this.state.buttonSearchClicked} /> : null}
         {this.state.buttonSearchClicked == true ? <SearchImages images={this.state.images} /> : null}
+
+        {this.state.buttonGameClicked == false ? <Play showGame={this.showGame} buttonGameClicked={this.state.buttonGameClicked} /> : null}
+        {this.state.buttonGameClicked == true ? <Game /> : null}
+
       </div>
     )
   }
+
+  fetchNews = (query = "") => {
+
+      $.ajax({
+          url: `https://node-hnapi.herokuapp.com/news`
+        }).then(json => {
+          this.setState({ items: json.map( item => item ) })
+        })
+
+        //I would figure out a way to search this API with more time, but I noticed other API sources of Hacker News have more searchable options. This particular one doesn't have a lot of options in the documentaiton for searching. I'd have to maybe look at the content of each item in the results dfrom the API to see if the title, for instance, includes the query from the search.
+
+    }
+
+//The function to call the API is called whewn the component mounts
+  componentDidMount() {
+      this.fetchNews()
+
+
+  }
+
 
 }
 
