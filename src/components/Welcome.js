87039dc02fb48fe93e4f8734ed2105b29afb522a
github username <github email address>
2019-03-04 22:35:36 -0500

    styling

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,9 +6,7 @@ export default class Welcome extends Component {
     return (
       <div className="navbar">
         <div class="navtitle">Welcome to the Universe</div>
-        <a href="emilyjennings.github.io">
-          <div className="emily">Made by Emily</div>
-        </a>
+          <div className="emily">An App Made by Emily</div>
       </div>
     );
   }
