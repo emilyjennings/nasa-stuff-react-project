ec62b8bb3f68e2dfdee92d3f30414dbee9a04b75
github username <github email address>
2019-03-06 11:32:24 -0500

    added comments to more stuff

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,33 +7,13 @@ class Search extends Component {
     query: ""
   }
 
-
-  // fetchImages(query) {
-  //   const url = "https://images-api.nasa.gov/search?q="
-  //   $.ajax({
-  //     url: url + query,
-  //     type: "GET",
-  //     dataType : "json",
-  //   }).done(function(json){
-  //     let imageres = json.collection.items[0].links[0].href
-  //   }).then(json => {
-  //     this.setState({ image: json.collection.items[0].links[0].href })
-  //   })
-  //   // fetch(`https://images-api.nasa.gov/search?q=${query}`)
-  //   // .then(res => res.json())
-  //   // .then(({data}) => {
-  //   //
-  //   //   this.setState({ image: data.collection.items[0].links[0].href })
-  //   //   this.setState({ desc: data.collection.items[0].data[0].description })
-  //   //   this.setState({ title: data.collection.items[0].data[0].title })
-  //   // })
-  // }
-
+  //This lets the SearchImages component know to use the query here for the search action
   handleSubmit = event => {
     event.preventDefault()
     this.props.fetchImages(this.state.query)
   }
 
+//renders a form to search and submits a query to the state
   render() {
       return (
         <div>
@@ -47,3 +27,27 @@ class Search extends Component {
 }
 
 export default Search
+
+
+//Code below is fore use later if I want to use fetch instead od ajax
+
+// fetchImages(query) {
+//   const url = "https://images-api.nasa.gov/search?q="
+//   $.ajax({
+//     url: url + query,
+//     type: "GET",
+//     dataType : "json",
+//   }).done(function(json){
+//     let imageres = json.collection.items[0].links[0].href
+//   }).then(json => {
+//     this.setState({ image: json.collection.items[0].links[0].href })
+//   })
+//   // fetch(`https://images-api.nasa.gov/search?q=${query}`)
+//   // .then(res => res.json())
+//   // .then(({data}) => {
+//   //
+//   //   this.setState({ image: data.collection.items[0].links[0].href })
+//   //   this.setState({ desc: data.collection.items[0].data[0].description })
+//   //   this.setState({ title: data.collection.items[0].data[0].title })
+//   // })
+// }
--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class SearchButton extends Component {
 
 
-
+//This component just renders a button to choose to search
   render() {
 
     return (
