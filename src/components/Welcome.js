8b3a5835acb971683c0720cd60ed2b1c41ead5fb
David Elster <davidandtheweb@gmail.com>
2019-03-18 21:06:29 -0400

    Fix: Errors/Warnings

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react'
 
 const Welcome = () => {
-
-
-    return (
-      <div className="navbar">
-        <div class="navtitle">Welcome to the Universe</div>
-          <div className="emily">An App Made by Emily</div>
-      </div>
-    );
-
+  return (
+    <div className="navbar">
+      <div class="navtitle">Welcome to the Universe</div>
+      <div className="emily">An App Made by Emily</div>
+    </div>
+  )
 }
 
 export default Welcome
